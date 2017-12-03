20ddd4836a22625ca8188fde703398b7d4c47a27
HeXiongjun <TonyHo.Profession@gmail.com>
2017-12-03 11:54:56 +0800

    It runs as expected: broast all recieved message to everyone

---
+++ build.cmd
@@ -0,0 +1,9 @@
+Net:
+    https://socket.io/get-started/chat/
+Install the packages:
+    sudo apt install nodejs npm
+    npm install --save express@4.15.2
+
+Run the example:
+    nodejs index.js
+
